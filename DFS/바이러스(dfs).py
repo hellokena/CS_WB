78942dd4fb4975811c82a5049de81e52f99fe21f
hellokena <42730820+hellokena@users.noreply.github.com>
2021-09-21 16:49:38 +0900

    Create 바이러스(dfs).py

---
+++ DFS/바이러스(dfs).py
@@ -0,0 +1,23 @@
+# 바이러스
+n = int(input()) # 컴퓨터 갯수
+m = int(input()) # 연결 쌍 갯수
+graph = [[0]*(n+1) for i in range(n+1)]
+visited = [0]*(n+1)
+
+for _ in range(m):
+    x,y = map(int, input().split())
+    graph[x][y] = graph[y][x] = 1
+
+cnt = 0
+
+def dfs(s):
+    global cnt
+    visited[s] = 1 # 방문 처리
+    for i in range(1, n+1):
+        if graph[s][i] == 1 and visited[i] == 0: # 방문하지 않은 노드가 있으면 dfs
+            dfs(i)
+            cnt += 1
+
+dfs(1) # 1번 컴퓨터 부터 시작
+# 시작점이 설정되어 있으므로 2중 for문 안 돌림
+print(cnt)
