5e14820f3d29e8d360c6dffdbb5fb075caa099ed
hellokena <42730820+hellokena@users.noreply.github.com>
2021-10-07 21:10:25 +0900

    Create COUNT_DISTINCT_중복제거하기.sql

---
+++ SQL/COUNT_DISTINCT_중복제거하기.sql
@@ -0,0 +1,7 @@
+-- ANIMAL_INS 테이블에서 동물의 이름은 몇개인지 확인하는 SQL
+-- 단, NULL은 집계하지 않으며, 중복되는 이름은 하나로 가정
+SELECT COUNT(DISTINCT NAME) AS 'count'
+FROM ANIMAL_INS;
+
+-- COUNT(*) 하면 NULL이 포함되지만
+-- COUNT(DISTINCT COL) 하면 NULL이 포함되지 않음
