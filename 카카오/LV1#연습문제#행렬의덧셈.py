ffec7207276003b91296bca09c5252b559d80250
hellokena <42730820+hellokena@users.noreply.github.com>
2021-10-10 23:00:37 +0900

    Update LV1#연습문제#행렬의덧셈.py

--- 카카오/LV1#연습문제#행렬의덧셈.py
+++ 카카오/LV1#연습문제#행렬의덧셈.py
@@ -6,3 +6,9 @@ def solution(arr1, arr2):
             tmp.append(a+b)
         answer.append(tmp)
     return answer
+
+import numpy as np
+def solution(arr1, arr2):
+    arr1 = np.array(arr1)
+    arr2 = np.array(arr2)
+    return (arr1+arr2).tolist()
