ef5731a73abc72693505fa752edc01b4c1628b08
hellokena <42730820+hellokena@users.noreply.github.com>
2021-12-31 23:24:51 +0900

    Update 백준#2941_크로아티아 알파벳.py

--- Implementation/백준#2941_크로아티아 알파벳.py
+++ Implementation/백준#2941_크로아티아 알파벳.py
@@ -8,3 +8,15 @@ def solution(s):
     
 s = input()
 solution(s)
+
+# -----------------
+
+# 크로아티아 알파벳
+def solution(s):
+    alphabet = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+    for a in alphabet:
+        s = s.replace(a, '.')
+    print(len(s))
+
+s = input()
+solution(s)
